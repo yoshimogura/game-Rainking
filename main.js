b1cf995c652e99f3087be2601e03c05338efe327
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-02-23 20:37:00 +0900

    2/23

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // ドキュメントが読みこまれたら実行する関数を定義
 window.onload = () => {
+  const up5game = games.filter((game) => game.rank <= 5);
   games.forEach((game) => {
-    addMarker(game);
+    addChart(game);
   });
+};
+const addChart = (game) => {
+  const parent = $("#chartTable");
+  console.log(game.name);
 };
