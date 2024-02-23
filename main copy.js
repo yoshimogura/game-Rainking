836d532f0278f7a3dce2764af51e01beca3fb618
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-02-24 00:25:32 +0900

    2/24

---
+++ main copy.js
@@ -0,0 +1,19 @@
+// ドキュメントが読みこまれたら実行する関数を定義
+window.onload = () => {
+  const up10game = games.filter((game) => game.rank <= 10);
+  up10game.forEach((game) => {
+    // console.log(game);
+    addChart(game);
+  });
+};
+
+const addChart = (game) => {
+  // $("#xxx").text(game.name);
+  // console.log(game.name);
+
+  const parent = $("#chartTable");
+  const li = $("<li>", { text: game.name });
+  parent.append(li);
+
+  // $("#station-info-title").text(station.name);
+};
--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 // ドキュメントが読みこまれたら実行する関数を定義
 window.onload = () => {
-  const up5game = games.filter((game) => game.rank <= 10);
-  games.forEach((game) => {
-    addChart(game);
+  const up5game = games.filter((game) => game.rank <= 5);
+  up5game.forEach((game) => {
+    // console.log(game);
+    addChart(game, 1);
+  });
+  const up6_10game = games.filter((game) => game.rank <= 10 && game.rank >= 6);
+  up6_10game.forEach((game) => {
+    console.log(game);
+    addChart(game, 2);
   });
 };
-const addChart = (game) => {
-  const parent = $("#chartTable");
 
-  const li = $("<li>", { text: game.name });
-  parent.append(li);
-  //console.log(game)
+const addChart = (game, no) => {
+  // $("#xxx").text(game.name); // aaa
+  console.log(no);
+
+  if (no == 1) {
+    const parent = $("#chartTable1");
+    const li = $("<li>", { text: game.name });
+    parent.append(li);
+  } else {
+    const parent = $("#chartTable2");
+    const li = $("<li>", { text: game.name });
+    parent.append(li);
+  }
+
+  // $("#station-info-title").text(station.name);
 };
